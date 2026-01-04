018d795db9939ce7b3b0791e602a95218e705d46
Mikołaj <dev.handbook870@slmails.com>
2026-01-04 15:19:01 +0100

    Set up basic app file structure

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, APIRouter
 
 
-app = FastAPI()
+app = FastAPI(title="Trackflow",
+              summary="A b2b platform for sharing track demos between labels and producers.",
+              version="0.0.1")
 
+api_router = APIRouter()
 
-@app.get("/", status_code=status.HTTP_200_OK)
+@api_router.get("/", status_code=status.HTTP_200_OK)
 async def root():
     return {"Hello": "world!"}
+
+app.include_router(router=api_router, prefix="/api")
