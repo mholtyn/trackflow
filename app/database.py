3e9bafa34d45e50eadb9668dc14dc71987b6c817
Mikołaj <dev.handbook870@slmails.com>
2026-02-15 11:59:08 +0100

    feat(database): add production engine

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from typing import AsyncGenerator
+import ssl
 
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.pool import NullPool
 
 from app.settings import settings
 
@@ -13,8 +15,13 @@ class Base(DeclarativeBase):
 if settings.app_env == "local":
     engine = create_async_engine(url=settings.database_url, echo=True)
 else:
-    #TODO: prod engine
-    pass
+    ssl_context = ssl.create_default_context()
+    engine = create_async_engine(
+        settings.database_url.split("?")[0],  # remove ?sslmode=require
+        echo=True,
+        poolclass=NullPool,
+        connect_args={"ssl": ssl_context},
+    )
 
 SessionLocal = async_sessionmaker(bind=engine, autoflush=True, expire_on_commit=False)
 
